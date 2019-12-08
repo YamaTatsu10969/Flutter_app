8126dd59729c2d5142d60684942d1fe3c0ab423a
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-08 23:23:49 +0900

    finish mono first article

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,65 @@
+// ※コメントは mono さんのブログ[https://medium.com/flutter-jp/first-step-9b7f2c74fb08] の引用がほとんどです
+// マテリアルデザイン用のUIコンポーネントを使えるように上のように import 文を書きます。
+// iOSっぽいデザインにしたい場合、以下が用意されていますが、とりあえずマテリアルデザインに統一した方が楽かつ簡単です。
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
+// FlutterのUI構築の1番の基本はStatelessWidgetを継承したクラスの組み合わせです。名前の通り、Stateless(状態を持たず静的)なWidget(UIコンポーネント)です。
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Hello Flutter!!!!!!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
+        textTheme: TextTheme(
+          display1: TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          )
+        )
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Hello Flutter!!!!!!'),
     );
   }
 }
 
+class yamatatsuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
 class MyHomePage extends StatefulWidget {
+  // コンストラクター
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
+  // G. 状態の保持と更新
+  int _counter = 0;
   void _incrementCounter() {
+    // 値の更新とともに、それをUIに伝えたい場合は、次のようにsetStateで囲むお作法があります。
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
@@ -92,11 +83,11 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Hello World!!!!!!!!!',
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              style: Theme.of(context).textTheme.display1
             ),
           ],
         ),
