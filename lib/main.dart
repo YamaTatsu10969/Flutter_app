9841e0a0925e5455dff46e62fdbee6779935e04a
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-12-09 23:21:38 +0900

    add Flutter Write your first Flutter app, part 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 // マテリアルデザイン用のUIコンポーネントを使えるように上のように import 文を書きます。
 // iOSっぽいデザインにしたい場合、以下が用意されていますが、とりあえずマテリアルデザインに統一した方が楽かつ簡単です。
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,90 +14,56 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello Flutter!!!!!!',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        textTheme: TextTheme(
-          display1: TextStyle(
-            fontSize: 72,
-            fontWeight: FontWeight.bold,
-          )
-        )
-      ),
-      home: MyHomePage(title: 'Hello Flutter!!!!!!'),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
 
-class yamatatsuApp extends StatelessWidget {
+class RandomWords extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Container();
+  RandomWordsState createState() => RandomWordsState();
+}
+
+//class RandomWords extends StatefulWidget {
+//  @override
+//  RandomWordsState createState() => RandomWordsState();
+//}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+
+          final index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+            }
+          return _buildRow(_suggestions[index]);
+        });
   }
-}
 
-
-class MyHomePage extends StatefulWidget {
-  // コンストラクター
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  // G. 状態の保持と更新
-  int _counter = 0;
-  void _incrementCounter() {
-    // 値の更新とともに、それをUIに伝えたい場合は、次のようにsetStateで囲むお作法があります。
-    setState(() {
-      _counter++;
-    });
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('Startup Name Generator'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World!!!!!!!!!',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: _buildSuggestions(),
     );
   }
 }
